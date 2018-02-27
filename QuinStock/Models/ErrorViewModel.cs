de56b4a96e8daaf06bbbf8547cfe094c528a4562
IrwanHamid <fieldengineerirwan@gmail.com>
2018-02-27 16:47:01 +0800

    Add project files.

---
+++ QuinStock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuinStock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
